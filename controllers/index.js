d80128688a0f60f40561bd021d2cb4986b104bb9
Jian Gong <git@jiangong.me>
2014-04-05 10:46:50 -0700

    add facebook friends info

--- controllers/index.js
+++ controllers/index.js
@@ -6,6 +6,7 @@ var passport = require('passport');
 var User = require('../models/User');
 var Grid = require('../models/Grid');
 var secrets = require('../config/secrets');
+var graph = require('fbgraph');
 
 /**
  * GET /index
@@ -27,10 +28,37 @@ exports.getIndex = function(req, res) {
         //return res.redirect('/');
       }
       console.log(docs);
-      res.render('user_index', {
-        title: 'Aggrid',
-        grids: docs
-      });
+
+      //facebook
+      var token = _.findWhere(req.user.tokens, { kind: 'facebook' });
+	  graph.setAccessToken(token.accessToken);
+	  var resInfo_FB = {};
+
+	  async.parallel({
+	    getMe: function(done) {
+	      graph.get(req.user.facebook, function(err, me) {
+	      	resInfo_FB.me = me;
+	        done(err, me);
+	      });
+	    },
+	    getMyFriends: function(done) {
+	      graph.get(req.user.facebook + '/friends', function(err, friends) {
+	      	resInfo_FB.b = friends.data;
+	        done(err, friends.data);
+	      });
+	    }
+	  },
+	  function(err, results) {
+	    if (err) return next(err);
+	    res.render('user_index', {
+          title: 'Aggrid',
+          grids: docs,
+          //me: results.getMe,
+          //friends: results.getMyFriends
+          resInfo_FB: resInfo_FB
+      	});
+	  });
+      
     });
   }
 };
